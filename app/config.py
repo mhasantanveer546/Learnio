d4ab4b07d760ea734c52fdb0d81061195993fbc6
mhasantanveer546 <mhasantanveer546@gmail.com>
2026-08-13 13:01:39 +0500

    small changes for better ux while hosting

--- app/config.py
+++ app/config.py
@@ -36,6 +36,7 @@ class Config:
     R2_ACCESS_KEY_ID = os.environ.get("R2_ACCESS_KEY_ID")
     R2_SECRET_ACCESS_KEY = os.environ.get("R2_SECRET_ACCESS_KEY")
     R2_BUCKET_NAME = os.environ.get("R2_BUCKET_NAME")
+    ASSET_VERSION = 2
 
 class DevelopmentConfig(Config):
     DEBUG = True
